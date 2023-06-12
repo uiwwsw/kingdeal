f55328415cf7a003571742b878fa4a0b6f0384f8
uiwwsw <uiwwsw@icloud.com>
2023-06-12 21:43:55 +0900

    chore: save

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,30 +9,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Map<String, List<dynamic>> data;
   Future<void> _pullRefresh() async {
-    ApiService().getDatas();
+    final res = await ApiService().getDatas();
+    print(res.keys);
+    // setState(() {
+    //   data = res;
+    // });
+  }
+
+  // List<dynamic> get products {
+  //   var d = [];
+  //   for (var element in data.values) {
+  //     d.add(element);
+  //   }
+  //   return d;
+  // }
+
+  _HomeScreenState() {
+    _pullRefresh();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.green,
-          title: const Text(
-            "킹받는 킹딜",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-          ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "킹받는 킹딜",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
         ),
-        body: RefreshIndicator(
-          onRefresh: _pullRefresh,
-          child: ListView(
-            children: const [
-              Text('ddd'),
-              Text('ddd'),
-            ],
-          ),
-        ));
+      ),
+      body: RefreshIndicator(
+        onRefresh: _pullRefresh,
+        child: ListView(
+            // children: List.from(products),
+            ),
+      ),
+    );
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,21 +1,23 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  void getDatas() async {
+  Map<String, List<dynamic>> toObject(String str) {
+    Map<String, dynamic> map = jsonDecode(str);
+    return Map.from(map);
+  }
+
+  Future<Map<String, List<dynamic>>> getDatas() async {
     final url = Uri.parse(
       '//kingdeal.duckdns.org/getDatas',
     );
-    print('ㄹ우하하');
     final response = await http
         .get(url, headers: {HttpHeaders.authorizationHeader: 'matthew1234'});
-    print(response);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      return toObject(response.body);
     }
-    print('오류');
     throw Error();
   }
 }
